e29882a4731f0ed9dbc782e82dc5443c846d7e4d
Peter <Peter.van.Loon@xs4all.nl>
2019-01-01 15:33:25 +0100

    Completed the reveal on scroll behaviour.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,7 @@
+import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 var mobileMenu = new MobileMenu();
+var revealOnScroll = new RevealOnScroll($(".feature-item"), "85%");
+var revealOnScroll = new RevealOnScroll($(".testimonial"), "60%");
